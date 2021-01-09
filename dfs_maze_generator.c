72577140a17fd6d01c12edfd30448d1d02ff78f3
Syhn <Mintish@users.noreply.github.com>
2021-01-08 20:02:38 -0600

    Refinements to maze generation - better backtracking - refinements to `pick_next_head`

--- dfs_maze_generator.c
+++ dfs_maze_generator.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "dfs_maze_generator.h"
 
-#define MAX_FRONTIER 10000
+#define MAX_FRONTIER 100
 
-void pick_next_head(int frontier_length, maze_tile_t *frontier[])
+maze_tile_t* pick_next_head(int frontier_length, maze_tile_t *frontier[])
 {
   int pick = rand() % frontier_length;
   maze_tile_t *picked_tile = frontier[pick];
-  frontier[pick] = frontier[frontier_length - 1];
-  frontier[frontier_length - 1] = picked_tile;
+  return picked_tile;
 }
 
 void generate_maze(maze_t *maze,
@@ -28,33 +28,21 @@ void generate_maze(maze_t *maze,
   maze_tile_t *deepest_tile;
   while (frontier_p >= 0) {
     maze_tile_t *head = frontier[frontier_p];
-    int frontier_length = frontier_p;
-    frontier_p--;
 
     maze_tile_t *neighbors[MAX_NEIGHBORS];
     int neighbors_length = get_neighbors(head, neighbors, maze);
 
     if (neighbors_length > 0) {
-      pick_next_head(neighbors_length, neighbors);
-
-      maze_tile_t *t_h;
-      maze_tile_t *t_c;
-      for (int v = 0; v < neighbors_length; v++) {
-        t_h = head;
-        t_c = neighbors[v];
-        frontier_p++;
-        if (v < neighbors_length - 1) {
-          // Re-add the current head as the back-track point
-          frontier[frontier_p] = t_h;
-        } else {
-          frontier[frontier_p] = t_c;
-        }
-      }
-
-      link_tiles(t_h, t_c, maze);
+      maze_tile_t *new_head = pick_next_head(neighbors_length, neighbors);
+      frontier_p++;
+      frontier[frontier_p] = new_head;
+      link_tiles(head, new_head, maze);
     } else if (frontier_p > max_depth) {
       deepest_tile = head;
       max_depth = frontier_p;
+      frontier_p--;
+    } else {
+      frontier_p--;
     }
   }
   mark_end_tile(deepest_tile);
